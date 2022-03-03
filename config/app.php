4b644a152fd403a20a120beb1cba1282880eeb4e
devops@izzi.studio <4WcvcRS6heLbkbM>
2022-03-03 14:41:08 +0200

    seo+multilang

--- config/app.php
+++ config/app.php
@@ -176,6 +176,7 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
+        Artesaos\SEOTools\Providers\SEOToolsServiceProvider::class
 
     ],
 
@@ -191,7 +192,7 @@ return [
     */
 
     'aliases' => Facade::defaultAliases()->merge([
-        // ...
+        'SEOMeta' => Artesaos\SEOTools\Facades\SEOMeta::class,
     ])->toArray(),
 
 ];
