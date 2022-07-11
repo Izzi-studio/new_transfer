3c6c7a5564dc5583971f731775054c1e2b47e256
kyslyi <inbox@izzi.studio>
2022-07-11 17:51:10 +0300

    add payment page into partner account

--- resources/js/profiles/firma/views/Payment.vue
+++ resources/js/profiles/firma/views/Payment.vue
@@ -1,3 +1,25 @@
 <template>
-    <p>Guthaben Aufladen</p>
+    <form id="payment-form" method="POST" :action="$store.state.additionalInfo.paymentUrl">
+        <h4 class="d-block d-lg-none mb-3">{{trans('top-up-bill')}}</h4>
+        <input type="hidden" name="_token" :value="csrfToken">
+        <div class="row">
+            <div class="col-sm-6">
+                <label class="form-field" for="accountholder-name">
+                    <p class="form-field__label ps-0">{{trans('total')}}</p>
+                    <input name="amount" type="text" required />
+                </label>
+            </div>
+        </div>
+        <input class="btn mt-4" type="submit" :value="trans('send')" />
+    </form>
 </template>
+
+<script>
+export default {
+    computed: {
+        csrfToken() {
+            return document.querySelector(`meta[name='csrf-token']`).content
+        }
+    },
+}
+</script>
--- resources/views/firma.blade.php
+++ resources/views/firma.blade.php
@@ -12,12 +12,13 @@
                     'reinigungUrl' => route('client.form.reinigung.view'),
                     'umzugUndReinigungUrl' => route('client.form.umzug_und_reinigung.view'),
                     'malarUrl' => route('client.form.malar.view'),
-                    'bodenlegerUrl' => route('client.form.bodenleger.iew'),
+                    'bodenlegerUrl' => route('client.form.bodenleger.view'),
                     'heizungUrl' => route('client.form.heizung.view'),
                     'elektrikerUrl' => route('client.form.elektriker.view'),
                     'gartnerUrl' => route('client.form.gartner.view'),
                     'schreinerUrl' => route('client.form.schreiner.view'),
                     'resellUrl' => route('partner.resell.view'),
+                    'paymentUrl' => route('api.payment-post'),
                 ))}}"
             />
         </div>
